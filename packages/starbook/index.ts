0d4c8a3231e636aae3d040375696c6bc5d5cfd51
Chris Swithinbank <swithinbank@gmail.com>
2023-03-18 22:57:53 +0100

    Add search to built site with pagefind

--- packages/starbook/index.ts
+++ packages/starbook/index.ts
@@ -1,4 +1,7 @@
 import type { AstroIntegration, AstroUserConfig, ViteUserConfig } from 'astro';
+import { spawn } from 'node:child_process';
+import { basename, dirname, relative } from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { StarbookConfig } from './types';
 
 const virtualModuleId = 'virtual:starbook/user-config';
@@ -21,6 +24,19 @@ export default function StarbookIntegration(
           },
         };
         updateConfig(newConfig);
+      },
+
+      'astro:build:done': ({ dir }) => {
+        const targetDir = fileURLToPath(dir);
+        const cwd = dirname(fileURLToPath(import.meta.url));
+        const relativeDir = relative(cwd, targetDir);
+        return new Promise<void>((resolve) => {
+          spawn('npx', ['-y', 'pagefind', '--source', relativeDir], {
+            stdio: 'inherit',
+            shell: true,
+            cwd,
+          }).on('close', () => resolve());
+        });
       },
     },
   };
