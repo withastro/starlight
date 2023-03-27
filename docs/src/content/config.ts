124316ff3eb6c37f6da5733ee75a4a0859235773
Chris Swithinbank <swithinbank@gmail.com>
2023-03-27 13:55:57 +0200

    Simplify usage of docs schema

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,6 +1,5 @@
-import { defineCollection } from 'astro:content';
-import schema from 'starbook/schema';
+import { defineDocsCollection } from 'starbook/schema';
 
 export const collections = {
-  docs: defineCollection({ schema }),
+  docs: defineDocsCollection(),
 };
--- packages/starbook/schema.ts
+++ packages/starbook/schema.ts
@@ -1,6 +1,10 @@
 import { z } from 'astro/zod';
 
-export default z.object({
-  title: z.string(),
-  description: z.string().optional(),
-});
+export function defineDocsCollection() {
+  return {
+    schema: z.object({
+      title: z.string(),
+      description: z.string().optional(),
+    }),
+  };
+}
