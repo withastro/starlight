e22dd76136f9749bb5d43f96241385faccfc90a1
Chris Swithinbank <swithinbank@gmail.com>
2023-05-12 23:53:55 +0200

    Add support for generating sitemap (#40)

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://starlight.astro.build',
   integrations: [
     starlight({
       title: 'Starlight Docs',
---
+++ packages/starlight/integrations/sitemap.ts
@@ -0,0 +1,22 @@
+import sitemap, { SitemapOptions } from '@astrojs/sitemap';
+import type { StarlightConfig } from '../types';
+
+/**
+ * A wrapped version of the `@astrojs/sitemap` integration configured based
+ * on Starlight i18n config.
+ */
+export function starlightSitemap(opts: StarlightConfig) {
+  const sitemapConfig: SitemapOptions = {};
+  if (opts.isMultilingual) {
+    sitemapConfig.i18n = {
+      defaultLocale: opts.defaultLocale.locale! || 'root',
+      locales: Object.fromEntries(
+        Object.entries(opts.locales).map(([locale, config]) => [
+          locale,
+          config?.lang!,
+        ])
+      ),
+    };
+  }
+  return sitemap(sitemapConfig);
+}
