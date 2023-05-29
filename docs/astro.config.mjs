2d517623fb8670d4e7f2656eacff0d5beb27d95a
Shinya Fujino <shf0811@gmail.com>
2023-05-30 05:44:05 +0900

    Add Japanese language support & translate some docs into Japanese (#97)
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -33,11 +33,12 @@ export default defineConfig({
         root: { label: 'English', lang: 'en' },
         de: { label: 'Deutsch', lang: 'de' },
         es: { label: 'Español', lang: 'es' },
+        ja: { label: '日本語', lang: 'ja' },
       },
       sidebar: [
         {
           label: 'Start Here',
-          translations: { de: 'Beginne hier', es: 'Comienza aqui' },
+          translations: { de: 'Beginne hier', es: 'Comienza aqui', ja: 'ここから始める' },
           items: [
             { label: 'Welcome, world', link: '/' },
             { label: 'Getting Started', link: 'getting-started' },
@@ -46,12 +47,12 @@ export default defineConfig({
         },
         {
           label: 'Guides',
-          translations: { de: 'Anleitungen', es: 'Guías' },
+          translations: { de: 'Anleitungen', es: 'Guías', ja: 'ガイド' },
           autogenerate: { directory: 'guides' },
         },
         {
           label: 'Reference',
-          translations: { de: 'Referenz', es: 'Referencias' },
+          translations: { de: 'Referenz', es: 'Referencias', ja: 'リファレンス' },
           autogenerate: { directory: 'reference' },
         },
       ],
--- packages/starlight/translations/index.ts
+++ packages/starlight/translations/index.ts
@@ -2,9 +2,10 @@ import { i18nSchema } from '../schemas/i18n';
 import en from './en.json';
 import es from './es.json';
 import de from './de.json';
+import ja from './ja.json';
 
 const parse = i18nSchema().required().strict().parse;
 
 export default Object.fromEntries(
-  Object.entries({ en, es, de }).map(([key, dict]) => [key, parse(dict)])
+  Object.entries({ en, es, de, ja }).map(([key, dict]) => [key, parse(dict)])
 );
