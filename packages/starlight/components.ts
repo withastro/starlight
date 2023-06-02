57492a60284b507832567ea806bff4b8fe49e7e8
Chris Swithinbank <swithinbank@gmail.com>
2023-06-03 01:43:27 +0200

    Expose `<Card>` and `<CardGrid>` components

--- packages/starlight/components.ts
+++ packages/starlight/components.ts
@@ -1,2 +1,4 @@
+export { default as Card } from './user-components/Card.astro';
+export { default as CardGrid } from './user-components/CardGrid.astro';
 export { default as Tabs } from './user-components/Tabs.astro';
 export { default as TabItem } from './user-components/TabItem.astro';
