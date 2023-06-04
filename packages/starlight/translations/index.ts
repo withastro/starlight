5f4acdf75102f4431f5c60f65912db8d690b098c
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-06-04 18:40:29 -0300

    Add Portuguese language support (#134)
    Co-authored-by: Chris Swithinbank <swithinbank@gmail.com>

--- packages/starlight/translations/index.ts
+++ packages/starlight/translations/index.ts
@@ -3,9 +3,10 @@ import en from './en.json';
 import es from './es.json';
 import de from './de.json';
 import ja from './ja.json';
+import pt from './pt.json';
 
 const parse = i18nSchema().required().strict().parse;
 
 export default Object.fromEntries(
-  Object.entries({ en, es, de, ja }).map(([key, dict]) => [key, parse(dict)])
+  Object.entries({ en, es, de, ja, pt }).map(([key, dict]) => [key, parse(dict)])
 );
