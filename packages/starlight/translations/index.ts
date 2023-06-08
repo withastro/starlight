1ec8457c6c4baaebf3a0d453bff76d9e2a0ce96c
Chris Swithinbank <swithinbank@gmail.com>
2023-06-08 20:07:56 +0200

    Add Pagefind translation keys to i18n schema

--- packages/starlight/translations/index.ts
+++ packages/starlight/translations/index.ts
@@ -1,4 +1,4 @@
-import { i18nSchema } from '../schemas/i18n';
+import { builtinI18nSchema } from '../schemas/i18n';
 import en from './en.json';
 import es from './es.json';
 import de from './de.json';
@@ -6,8 +6,15 @@ import ja from './ja.json';
 import pt from './pt.json';
 import fr from './fr.json';
 
-const parse = i18nSchema().required().strict().parse;
+const { parse } = builtinI18nSchema();
 
 export default Object.fromEntries(
-  Object.entries({ en, es, de, ja, pt, fr }).map(([key, dict]) => [key, parse(dict)])
+  Object.entries({
+    en,
+    es,
+    de,
+    ja,
+    pt,
+    fr,
+  }).map(([key, dict]) => [key, parse(dict)])
 );
