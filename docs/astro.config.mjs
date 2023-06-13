6bbe751c270c6a56effea6aac193aa85b8fbdc44
Waxer59 <hnboyano@gmail.com>
2023-06-14 01:18:44 +0200

    use locales in translation-status

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,98 +1,106 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
-const site = 'https://starlight.astro.build/';
+export const locales = {
+  root: { label: 'English', lang: 'en' },
+  de: { label: 'Deutsch', lang: 'de' },
+  es: { label: 'Español', lang: 'es' },
+  ja: { label: '日本語', lang: 'ja' },
+  fr: { label: 'Français', lang: 'fr' },
+  it: { label: 'Italiano', lang: 'it' },
+};
+
+const site = "https://starlight.astro.build/";
 
 export default defineConfig({
   site,
   integrations: [
     starlight({
-      title: 'Starlight',
+      title: "Starlight",
       logo: {
-        light: '/src/assets/logo-light.svg',
-        dark: '/src/assets/logo-dark.svg',
-        replacesTitle: true,
+        light: "/src/assets/logo-light.svg",
+        dark: "/src/assets/logo-dark.svg",
+        replacesTitle: true
       },
       editLink: {
-        baseUrl: 'https://github.com/withastro/starlight/edit/main/docs/',
+        baseUrl: "https://github.com/withastro/starlight/edit/main/docs/"
       },
       social: {
-        github: 'https://github.com/withastro/starlight',
-        discord: 'https://astro.build/chat',
+        github: "https://github.com/withastro/starlight",
+        discord: "https://astro.build/chat"
       },
       head: [
         {
-          tag: 'script',
+          tag: "script",
           attrs: {
-            src: 'https://cdn.usefathom.com/script.js',
-            'data-site': 'EZBHTSIG',
-            defer: true,
-          },
+            src: "https://cdn.usefathom.com/script.js",
+            "data-site": "EZBHTSIG",
+            defer: true
+          }
         },
         {
-          tag: 'meta',
-          attrs: { property: 'og:image', content: site + 'og.jpg?v=1' },
+          tag: "meta",
+          attrs: { property: "og:image", content: site + "og.jpg?v=1" }
         },
         {
-          tag: 'meta',
-          attrs: { property: 'twitter:image', content: site + 'og.jpg?v=1' },
-        },
+          tag: "meta",
+          attrs: { property: "twitter:image", content: site + "og.jpg?v=1" }
+        }
       ],
-      customCss: process.env.NO_GRADIENTS ? [] : ['/src/assets/landing.css'],
-      locales: {
-        root: { label: 'English', lang: 'en' },
-        de: { label: 'Deutsch', lang: 'de' },
-        es: { label: 'Español', lang: 'es' },
-        ja: { label: '日本語', lang: 'ja' },
-        fr: { label: 'Français', lang: 'fr' },
-      },
+      customCss: process.env.NO_GRADIENTS ? [] : ["/src/assets/landing.css"],
+      locales,
       sidebar: [
         {
-          label: 'Start Here',
+          label: "Start Here",
           translations: {
-            de: 'Beginne hier',
-            es: 'Comienza aqui',
-            ja: 'ここからはじめる',
-            fr: 'Commencez ici',
+            de: "Beginne hier",
+            es: "Comienza aqui",
+            ja: "ここからはじめる",
+            fr: "Commencez ici"
           },
           items: [
             {
-              label: 'Getting Started',
-              link: 'getting-started',
+              label: "Getting Started",
+              link: "getting-started",
               translations: {
-                de: 'Erste Schritte',
-                es: 'Empezando',
-                ja: '入門',
-                fr: 'Mise en route'
-              },
+                de: "Erste Schritte",
+                es: "Empezando",
+                ja: "入門",
+                fr: "Mise en route"
+              }
             },
             {
-              label: 'Environmental Impact',
-              link: 'environmental-impact',
+              label: "Environmental Impact",
+              link: "environmental-impact",
               translations: {
-                es: 'Documentación ecológica',
-                ja: '環境への負荷',
-                fr: 'Impact environnemental'
-              },
-            },
-          ],
+                es: "Documentación ecológica",
+                ja: "環境への負荷",
+                fr: "Impact environnemental"
+              }
+            }
+          ]
         },
         {
-          label: 'Guides',
-          translations: { de: 'Anleitungen', es: 'Guías', ja: 'ガイド', fr: 'Guides' },
-          autogenerate: { directory: 'guides' },
-        },
-        {
-          label: 'Reference',
+          label: "Guides",
           translations: {
-            de: 'Referenz',
-            es: 'Referencias',
-            ja: 'リファレンス',
-            fr: 'Référence',
+            de: "Anleitungen",
+            es: "Guías",
+            ja: "ガイド",
+            fr: "Guides"
           },
-          autogenerate: { directory: 'reference' },
+          autogenerate: { directory: "guides" }
         },
-      ],
-    }),
-  ],
+        {
+          label: "Reference",
+          translations: {
+            de: "Referenz",
+            es: "Referencias",
+            ja: "リファレンス",
+            fr: "Référence"
+          },
+          autogenerate: { directory: "reference" }
+        }
+      ]
+    })
+  ]
 });
