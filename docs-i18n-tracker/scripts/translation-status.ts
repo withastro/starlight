34dc21f5f68f9099ffbf193a60e538090df66899
Chris Swithinbank <swithinbank@gmail.com>
2023-06-14 12:38:58 +0200

    Move tracker to own sub-package

---
+++ docs-i18n-tracker/scripts/translation-status.ts
@@ -0,0 +1,22 @@
+import { TranslationStatusBuilder } from './lib/translation-status/builder';
+import { locales } from '../../docs/astro.config.mjs';
+
+const translationStatusBuilder = new TranslationStatusBuilder({
+  pageSourceDir: '../docs/src/content/docs',
+  htmlOutputFilePath: './dist/index.html',
+  sourceLanguage: 'en',
+  targetLanguages: Object.values(locales)
+    .map((el) => el.lang)
+    .filter((lang) => lang !== 'en')
+    .sort(),
+  languageLabels: Object.values(locales).reduce((acc, curr) => {
+    return {
+      [curr.lang]: curr.label,
+      ...acc,
+    };
+  }, {}),
+  githubRepo: process.env.GITHUB_REPOSITORY || 'withastro/starlight',
+  githubToken: process.env.GITHUB_TOKEN,
+});
+
+await translationStatusBuilder.run();
