09a49265c77d34713e3df06a6cc9e359d7d748e7
Josef Ježek <josef@jezek.co.cz>
2023-06-27 18:43:21 +0200

    i18n(cs): Add cs.json

--- packages/starlight/translations/index.ts
+++ packages/starlight/translations/index.ts
@@ -1,4 +1,5 @@
 import { builtinI18nSchema } from '../schemas/i18n';
+import cs from './cs.json';
 import en from './en.json';
 import es from './es.json';
 import de from './de.json';
@@ -10,5 +11,5 @@ import it from './it.json';
 const { parse } = builtinI18nSchema();
 
 export default Object.fromEntries(
-  Object.entries({ en, es, de, ja, pt, fr, it }).map(([key, dict]) => [key, parse(dict)])
+  Object.entries({ cs, en, es, de, ja, pt, fr, it }).map(([key, dict]) => [key, parse(dict)])
 );
