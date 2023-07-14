40765870f55adf2f54dc1d799eeaa7edf3c8a72a
Chris Swithinbank <swithinbank@gmail.com>
2023-07-14 18:46:58 +0200

    Add tailwind starter project/example

---
+++ examples/tailwind/astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'Docs',
+      social: {
+        github: 'https://github.com/withastro/starlight',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+      /* TODO: Remove custom CSS once https://github.com/withastro/astro/issues/7563 is fixed. */
+      customCss: ['./src/tailwind.css'],
+    }),
+    /* TODO: Unset `applyBaseStyles` once https://github.com/withastro/astro/issues/7563 is fixed. */
+    tailwind({ applyBaseStyles: false }),
+  ],
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
+});
