d4c712510d343f62ecd7df358edbd5a834d6c53b
Chris Swithinbank <swithinbank@gmail.com>
2023-07-22 23:09:08 +0200

    Fix edge case bug in slug utility

--- packages/starlight/utils/slugs.ts
+++ packages/starlight/utils/slugs.ts
@@ -56,7 +56,7 @@ export function slugToParam(slug: string): string | undefined {
   return slug === 'index' || slug === ''
     ? undefined
     : slug.endsWith('/index')
-    ? slug.replace('/index', '')
+    ? slug.replace(/\/index$/, '')
     : slug;
 }
 
