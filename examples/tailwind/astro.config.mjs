5c56f25909a10bac90ef01787e8e20ce91f4e178
Chris Swithinbank <swithinbank@gmail.com>
2023-08-05 00:33:22 +0200

    Run Prettier

--- examples/tailwind/astro.config.mjs
+++ examples/tailwind/astro.config.mjs
@@ -4,35 +4,35 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Docs',
-      social: {
-        github: 'https://github.com/withastro/starlight',
-      },
-      sidebar: [
-        {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
-        },
-        {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
-        },
-      ],
-      /* TODO: Remove custom CSS once https://github.com/withastro/astro/issues/7563 is fixed. */
-      customCss: ['./src/tailwind.css'],
-    }),
-    /* TODO: Unset `applyBaseStyles` once https://github.com/withastro/astro/issues/7563 is fixed. */
-    tailwind({ applyBaseStyles: false }),
-  ],
-  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
-    },
-  },
+	integrations: [
+		starlight({
+			title: 'Docs',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			/* TODO: Remove custom CSS once https://github.com/withastro/astro/issues/7563 is fixed. */
+			customCss: ['./src/tailwind.css'],
+		}),
+		/* TODO: Unset `applyBaseStyles` once https://github.com/withastro/astro/issues/7563 is fixed. */
+		tailwind({ applyBaseStyles: false }),
+	],
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/sharp',
+		},
+	},
 });
