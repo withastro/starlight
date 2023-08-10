555826d39edec9b0535edf734656dd9bf7cc31ea
Chris Swithinbank <swithinbank@gmail.com>
2023-08-10 11:15:54 +0200

    Add a Tailwind plugin and docs (#337)

---
+++ examples/tailwind/astro.config.mjs
@@ -0,0 +1,36 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Docs with Tailwind',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/sharp',
+		},
+	},
+});
