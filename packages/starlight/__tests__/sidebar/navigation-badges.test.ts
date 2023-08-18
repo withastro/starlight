028448b30bcfabeeaac6e02e4bed01024fa0b664
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-08-18 17:40:48 -0400

    add test

---
+++ packages/starlight/__tests__/sidebar/navigation-badges.test.ts
@@ -0,0 +1,78 @@
+import { describe, expect, test, vi } from 'vitest';
+import { getSidebar } from '../../utils/navigation';
+
+vi.mock('astro:content', async () =>
+	(await import('../test-utils')).mockedAstroContent({
+		docs: [
+			['index.mdx', { title: 'Home Page' }],
+			['environmental-impact.md', { title: 'Eco-friendly docs' }],
+			['reference/configuration.md', { title: 'Config Reference', sidebar: {
+        badge: {
+          text: 'Experimental',
+          variant: 'purple',
+        }
+      } }],
+			['reference/frontmatter.md', { title: 'Frontmatter Reference', sidebar: { badge: "New" } }],
+			['guides/components.mdx', { title: 'Components' }],
+		],
+	})
+);
+
+describe('getSidebar', () => {
+	test('adds a badge object to the sidebar when using a "string" or "object"', () => {
+		expect(getSidebar('/', undefined)).toMatchInlineSnapshot(`
+			[
+			  {
+			    "href": "/",
+			    "isCurrent": true,
+			    "label": "Home",
+			    "type": "link",
+			  },
+			  {
+			    "collapsed": false,
+			    "entries": [
+			      {
+			        "href": "/intro/",
+			        "isCurrent": false,
+			        "label": "Introduction",
+			        "type": "link",
+			      },
+			      {
+			        "href": "/next-steps/",
+			        "isCurrent": false,
+			        "label": "Next Steps",
+			        "type": "link",
+			      },
+			    ],
+			    "label": "Start Here",
+			    "type": "group",
+			  },
+			  {
+			    "collapsed": false,
+			    "entries": [
+			      {
+			        "badge": {
+			          "text": "Experimental",
+			          "variant": "purple",
+			        },
+			        "href": "/reference/configuration/",
+			        "isCurrent": false,
+			        "label": "Config Reference",
+			        "type": "link",
+			      },
+			      {
+			        "badge": {
+			          "text": "New",
+			          "variant": "blue",
+			        },
+			        "href": "/reference/frontmatter/",
+			        "isCurrent": false,
+			        "label": "Frontmatter Reference",
+			        "type": "link",
+			      },
+			    ],
+			    "label": "Reference",
+			    "type": "group",
+			  },
+			]
+		`)})});
