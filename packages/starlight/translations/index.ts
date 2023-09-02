419d709d6ce5701725ea845079140c7a622fd845
ohadk <ohadkachlon@gmail.com>
2023-09-02 04:56:34 +0300

    add Hebrew translation

--- packages/starlight/translations/index.ts
+++ packages/starlight/translations/index.ts
@@ -1,26 +1,27 @@
 import { builtinI18nSchema } from '../schemas/i18n';
+import ar from './ar.json';
 import cs from './cs.json';
+import da from './da.json';
+import de from './de.json';
 import en from './en.json';
 import es from './es.json';
-import de from './de.json';
-import ja from './ja.json';
-import pt from './pt.json';
 import fa from './fa.json';
 import fr from './fr.json';
+import he from './he.json';
 import it from './it.json';
-import nl from './nl.json';
-import da from './da.json';
-import tr from './tr.json';
-import ar from './ar.json';
-import nb from './nb.json';
-import zh from './zh.json';
+import ja from './ja.json';
 import ko from './ko.json';
+import nb from './nb.json';
+import nl from './nl.json';
+import pt from './pt.json';
 import sv from './sv.json';
+import tr from './tr.json';
+import zh from './zh.json';
 
 const { parse } = builtinI18nSchema();
 
 export default Object.fromEntries(
-	Object.entries({ cs, en, es, de, ja, pt, fa, fr, it, nl, da, tr, ar, nb, zh, ko, sv }).map(
+	Object.entries({ ar, cs, da, de, en, es, fa, fr, he, it, ja, ko, nb, nl, pt, sv, tr, zh }).map(
 		([key, dict]) => [key, parse(dict)]
 	)
 );
