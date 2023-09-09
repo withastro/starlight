37184197ea7794e266783b37913d2dba72bf294d
Alex Bosch <abosch19@gmail.com>
2023-09-09 20:48:58 +0800

    feat: view transition in user config

--- packages/starlight/utils/user-config.ts
+++ packages/starlight/utils/user-config.ts
@@ -223,6 +223,12 @@ const UserConfigSchema = z.object({
 	 */
 	defaultLocale: z.string().optional(),
 
+	/**
+	 * Set ViewTransitions Astro component for this page.
+	 * 
+	 */
+	viewTransitions: z.boolean().optional().default(false),
+
 	/** Configure your site’s sidebar navigation items. */
 	sidebar: SidebarItemSchema.array().optional(),
 
