760e24b316a8b82545d7661c1630a77fac825674
Chris Swithinbank <swithinbank@gmail.com>
2023-09-19 16:00:28 +0200

    Fix import path after merge

--- packages/starlight/__tests__/basics/toc.test.ts
+++ packages/starlight/__tests__/basics/toc.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { generateToC } from '../../components/TableOfContents/generateToC';
+import { generateToC } from '../../utils/generateToC';
 
 const defaultOpts = { minHeadingLevel: 2, maxHeadingLevel: 3, title: 'Overview' };
 
