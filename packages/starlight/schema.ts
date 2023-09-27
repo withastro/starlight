2590f854d5a25b67b7d50d385550118048f8661a
Chris Swithinbank <swithinbank@gmail.com>
2023-09-27 22:40:55 +0200

    Only import type from astro:content

--- packages/starlight/schema.ts
+++ packages/starlight/schema.ts
@@ -1,4 +1,5 @@
-import { z, type SchemaContext } from 'astro:content';
+import { z } from 'astro/zod';
+import type { SchemaContext } from 'astro:content';
 import { HeadConfigSchema } from './schemas/head';
 import { PrevNextLinkConfigSchema } from './schemas/prevNextLink';
 import { TableOfContentsSchema } from './schemas/tableOfContents';
