15bd1fb55728f14effc8f77e80a4331bbdf69d8c
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-11-01 19:40:07 -0700

    add build to test config

--- packages/starlight/__tests__/test-config.ts
+++ packages/starlight/__tests__/test-config.ts
@@ -4,11 +4,15 @@ import { getViteConfig } from 'astro/config';
 import type { z } from 'astro/zod';
 import { vitePluginStarlightUserConfig } from '../integrations/virtual-user-config';
 import { StarlightConfigSchema } from '../utils/user-config';
+import type { AstroConfig } from 'astro';
 
 export function defineVitestConfig(config: z.input<typeof StarlightConfigSchema>) {
 	const root = new URL('./', import.meta.url);
 	const srcDir = new URL('./src/', root);
+	const build = {
+		format: "directory"
+	} as AstroConfig['build']
 	return getViteConfig({
-		plugins: [vitePluginStarlightUserConfig(StarlightConfigSchema.parse(config), { root, srcDir })],
+		plugins: [vitePluginStarlightUserConfig(StarlightConfigSchema.parse(config), { root, srcDir, build })],
 	});
 }
