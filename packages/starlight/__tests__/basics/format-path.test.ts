c04e4dd9b20cbe0a84ec36ba16123f831d04fb5d
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2023-11-03 14:06:46 -0700

    add test for pathformat

---
+++ packages/starlight/__tests__/basics/format-path.test.ts
@@ -0,0 +1,255 @@
+import { describe, expect, test } from 'vitest';
+import { formatPath } from '../../utils/format';
+
+describe('formatPath() - { format: "file" - trailingSlash: "ignore" }', () => {
+
+	const opts = {
+		format: 'file',
+		trailingSlash: 'ignore',
+	} as const;
+
+	const testCases = [
+		{
+			// index page
+			path: '/',
+			expected: '/index.html',
+		},
+		{
+			// with trailing slash
+			path: '/reference/configuration/',
+			expected: '/reference/configuration.html',
+		},
+		{
+			// without trailing slash
+			path: '/api/v1/users',
+			expected: '/api/v1/users.html',
+		},
+		{
+			// with file extension
+			path: '/guides/components.html',
+			expected: '/guides/components.html',
+		},
+		{
+			// with file extension and trailing slash
+			path: '/guides/components.html/',
+			expected: '/guides/components.html',
+		},
+	];
+
+	testCases.forEach(({ path, expected }) => {
+		test(`returns ${expected} for ${path}`, () => {
+			expect(formatPath(path, opts)).toBe(expected);
+		});
+	});
+});
+
+describe('formatPath() - { format: "file" , trailingSlash: "always" }', () => {
+
+	const opts = {
+		format: 'file',
+		trailingSlash: 'always',
+	} as const;
+
+	const testCases = [
+		{
+			// index page
+			path: '/',
+			expected: '/index.html/',
+		},
+		{
+			// with trailing slash
+			path: '/reference/configuration/',
+			expected: '/reference/configuration.html/',
+		},
+		{
+			// without trailing slash
+			path: '/api/v1/users',
+			expected: '/api/v1/users.html/',
+		},
+		{
+			// with file extension
+			path: '/guides/components.html',
+			expected: '/guides/components.html/',
+		},
+		{
+			// with file extension and trailing slash
+			path: '/guides/components.html/',
+			expected: '/guides/components.html/',
+		},
+	];
+
+	testCases.forEach(({ path, expected }) => {
+		test(`returns ${expected} for ${path}`, () => {
+			expect(formatPath(path, opts)).toBe(expected);
+		});
+	});
+});
+
+
+describe('formatPath() - { format: "file" , trailingSlash: "never" }', () => {
+
+	const opts = {
+		format: 'file',
+		trailingSlash: 'never',
+	} as const;
+
+	const testCases = [
+		{
+			// index page
+			path: '/',
+			expected: '/index.html',
+		},
+		{
+			// with trailing slash
+			path: '/reference/configuration/',
+			expected: '/reference/configuration.html',
+		},
+		{
+			// without trailing slash
+			path: '/api/v1/users',
+			expected: '/api/v1/users.html',
+		},
+		{
+			// with file extension
+			path: '/guides/components.html',
+			expected: '/guides/components.html',
+		},
+		{
+			// with file extension and trailing slash
+			path: '/guides/components.html/',
+			expected: '/guides/components.html',
+		},
+	];
+
+	testCases.forEach(({ path, expected }) => {
+		test(`returns ${expected} for ${path}`, () => {
+			expect(formatPath(path, opts)).toBe(expected);
+		});
+	});
+});
+
+describe('formatPath() - { format: "directory" , trailingSlash: "always" }', () => {
+
+	const opts = {
+		format: 'directory',
+		trailingSlash: 'always',
+	} as const;
+
+	const testCases = [
+		{
+			// index page
+			path: '/',
+			expected: '/',
+		},
+		{
+			// with trailing slash
+			path: '/reference/configuration/',
+			expected: '/reference/configuration/',
+		},
+		{
+			// without trailing slash
+			path: '/api/v1/users',
+			expected: '/api/v1/users/',
+		},
+		{
+			// with file extension
+			path: '/guides/components.html',
+			expected: '/guides/components/',
+		},
+		{
+			// with file extension and trailing slash
+			path: '/guides/components.html/',
+			expected: '/guides/components/',
+		},
+	];
+
+	testCases.forEach(({ path, expected }) => {
+		test(`returns ${expected} for ${path}`, () => {
+			expect(formatPath(path, opts)).toBe(expected);
+		});
+	});
+});
+
+describe('formatPath() - { format: "directory" , trailingSlash: "never" }', () => {
+
+	const opts = {
+		format: 'directory',
+		trailingSlash: 'never',
+	} as const;
+
+	const testCases = [
+		{
+			// index page
+			path: '/',
+			expected: '/',
+		},
+		{
+			// with trailing slash
+			path: '/reference/configuration/',
+			expected: '/reference/configuration',
+		},
+		{
+			// without trailing slash
+			path: '/api/v1/users',
+			expected: '/api/v1/users',
+		},
+		{
+			// with file extension
+			path: '/guides/components.html',
+			expected: '/guides/components',
+		},
+		{
+			// with file extension and trailing slash
+			path: '/guides/components.html/',
+			expected: '/guides/components',
+		},
+	];
+
+	testCases.forEach(({ path, expected }) => {
+		test(`returns ${expected} for ${path}`, () => {
+			expect(formatPath(path, opts)).toBe(expected);
+		});
+	});
+});
+
+describe('formatPath() - { format: "directory" , trailingSlash: "ignore" }', () => {
+
+	const opts = {
+		format: 'directory',
+		trailingSlash: 'ignore',
+	} as const;
+
+	const testCases = [
+		{
+			// index page
+			path: '/',
+			expected: '/',
+		},
+		{
+			// with trailing slash
+			path: '/reference/configuration/',
+			expected: '/reference/configuration',
+		},
+		{
+			// without trailing slash
+			path: '/api/v1/users',
+			expected: '/api/v1/users',
+		},
+		{
+			// with file extension
+			path: '/guides/components.html',
+			expected: '/guides/components',
+		},
+		{
+			// with file extension and trailing slash
+			path: '/guides/components.html/',
+			expected: '/guides/components',
+		},
+	];
+
+	testCases.forEach(({ path, expected }) => {
+		test(`returns ${expected} for ${path}`, () => {
+			expect(formatPath(path, opts)).toBe(expected);
+		});
+	});
+});
---
+++ packages/starlight/utils/format.ts
@@ -0,0 +1,47 @@
+import type { AstroConfig } from 'astro';
+import { fileWithBase, pathWithBase } from './base';
+import { stripExtension, ensureHtmlExtension } from './navigation';
+import { ensureTrailingSlash, stripTrailingSlash } from './path';
+
+interface FormatPathOptions {
+	format?: AstroConfig['build']['format'];
+	trailingSlash?: AstroConfig['trailingSlash'];
+}
+
+const formatStrategies = {
+	file: {
+		addBase: fileWithBase,
+		handleExtension: (href: string) => ensureHtmlExtension(href),
+	},
+	directory: {
+		addBase: pathWithBase,
+		handleExtension: (href: string) => stripExtension(href),
+	},
+};
+
+const trailingSlashStrategies = {
+	always: ensureTrailingSlash,
+	never: stripTrailingSlash,
+	ignore: (href: string) => href,
+};
+
+export function formatPath(href: string, opts?: FormatPathOptions) {
+  const options = {
+    format: opts?.format ?? 'directory',
+    trailingSlash: opts?.trailingSlash ?? 'ignore',
+  };
+
+  const formatStrategy = formatStrategies[options.format];
+	const trailingSlashStrategy = trailingSlashStrategies[options.trailingSlash];
+
+  // Add base
+  href = formatStrategy.addBase(href);
+
+  // Handle extension
+  href = formatStrategy.handleExtension(href);
+
+  // Handle trailing slash
+  href = href ? trailingSlashStrategy(href) : '/';
+
+  return href;
+}
