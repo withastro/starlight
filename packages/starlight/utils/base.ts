a8cc60001952ac8d1cc1106bc1b29a57f22cc536
Chris Swithinbank <swithinbank@gmail.com>
2023-11-16 19:25:13 +0100

    Make base utils unopinionated about trailing slashes

--- packages/starlight/utils/base.ts
+++ packages/starlight/utils/base.ts
@@ -1,15 +1,15 @@
-import { stripLeadingAndTrailingSlashes, stripTrailingSlash } from './path';
+import { stripLeadingSlash, stripTrailingSlash } from './path';
 
 const base = stripTrailingSlash(import.meta.env.BASE_URL);
 
 /** Get the a root-relative URL path with the site’s `base` prefixed. */
 export function pathWithBase(path: string) {
-	path = stripLeadingAndTrailingSlashes(path);
-	return path ? base + '/' + path + '/' : base + '/';
+	path = stripLeadingSlash(path);
+	return path ? base + '/' + path : base + '/';
 }
 
 /** Get the a root-relative file URL path with the site’s `base` prefixed. */
 export function fileWithBase(path: string) {
-	path = stripLeadingAndTrailingSlashes(path);
+	path = stripLeadingSlash(path);
 	return path ? base + '/' + path : base;
 }
