051216316be56aebb8b7f1a6050310b40d97112b
Chris Swithinbank <swithinbank@gmail.com>
2023-11-29 21:21:51 +0100

    Delete leftover code from plugins branch

--- packages/starlight-search-demo/virtual.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'virtual:starlight-search-demo-config' {
-	const StarlightSearchDemoConfig: import('./index').StarlightSearchDemoConfig;
-
-	export default StarlightSearchDemoConfig;
-}
