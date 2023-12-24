b19e0d1a0191434557467388af1c85e7e97f44e5
Yan Thomas <61414485+Yan-Thomas@users.noreply.github.com>
2023-12-24 17:22:08 -0300

    Migrate localization tooling to Lunaria

---
+++ docs/lunaria/components.ts
@@ -0,0 +1,15 @@
+import { html } from '@lunariajs/core';
+
+export const TitleParagraph = () => html`
+	<p>
+		If you're interested in helping us translate
+		<a href="https://starlight.astro.build/">starlight.astro.build</a> into one of the languages
+		listed below, you've come to the right place! This auto-updating page always lists all the
+		content that could use your help right now.
+	</p>
+	<p>
+		Before starting a new translation, please read our
+		<a href="https://contribute.docs.astro.build/guides/i18n/">i18n Guide</a>
+		to learn about our translation process and how you can get involved.
+	</p>
+`;
