de2e5967f23c48ea5442a80b40f249592699aba5
hippotastic <6137925+hippotastic@users.noreply.github.com>
2024-01-25 22:25:15 +0100

    Add comment explaining Windows fix in pathToLocale

--- packages/starlight/integrations/shared/pathToLocale.ts
+++ packages/starlight/integrations/shared/pathToLocale.ts
@@ -24,6 +24,8 @@ export function pathToLocale(
 	const docsDir = new URL('content/docs/', srcDir);
 	// Format path to unix style path.
 	path = path?.replace(/\\/g, '/')
+	// Ensure that the page path starts with a slash if the docs directory also does,
+	// which makes stripping the docs path in the next step work on Windows, too.
 	if (path && !path.startsWith('/') && docsDir.pathname.startsWith('/')) path = '/' + path;
 	// Strip docs path leaving only content collection file ID.
 	// Example: /Users/houston/repo/src/content/docs/en/guide.md => en/guide.md
