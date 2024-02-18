e4c1c2cdde8ac57ab3a5af5c2052b3365adca4b7
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-02-18 18:36:21 +0100

    feat: export `StarlightPageProps` type

--- packages/starlight/types.ts
+++ packages/starlight/types.ts
@@ -3,3 +3,4 @@ export type {
 	StarlightPlugin,
 	StarlightUserConfigWithPlugins as StarlightUserConfig,
 } from './utils/plugins';
+export type { StarlightPageProps } from './utils/starlight-page';
