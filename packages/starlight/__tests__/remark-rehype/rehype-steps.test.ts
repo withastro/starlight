9e82011cf1022bd712e9c1da123efc1617145904
Chris Swithinbank <swithinbank@gmail.com>
2024-02-27 16:38:02 +0100

    Also fix snapshots

--- packages/starlight/__tests__/remark-rehype/rehype-steps.test.ts
+++ packages/starlight/__tests__/remark-rehype/rehype-steps.test.ts
@@ -26,17 +26,17 @@ test('component with non-element content throws an error', () => {
 test('component with non-`<ol>` content throws an error', () => {
 	expect(() => processSteps('<p>A paragraph is not an ordered list</p>'))
 		.toThrowErrorMatchingInlineSnapshot(`
-		"[AstroUserError]:
-			The \`<FileTree>\` component expects its content to be a single ordered list (\`<ol>\`) but found the following element: \`<p>\`.
-		Hint:
-			To learn more about the \`<Steps>\` component, see https://starlight.astro.build/guides/components/#steps"
-	`);
+			"[AstroUserError]:
+				The \`<Steps>\` component expects its content to be a single ordered list (\`<ol>\`) but found the following element: \`<p>\`.
+			Hint:
+				To learn more about the \`<Steps>\` component, see https://starlight.astro.build/guides/components/#steps"
+		`);
 });
 
 test('component with multiple children throws an error', () => {
 	expect(() => processSteps('<ol></ol><ol></ol>')).toThrowErrorMatchingInlineSnapshot(`
 		"[AstroUserError]:
-			The \`<FileTree>\` component expects its content to be a single ordered list (\`<ol>\`) but found multiple child elements: \`<ol>\`, \`<ol>\`.
+			The \`<Steps>\` component expects its content to be a single ordered list (\`<ol>\`) but found multiple child elements: \`<ol>\`, \`<ol>\`.
 		Hint:
 			To learn more about the \`<Steps>\` component, see https://starlight.astro.build/guides/components/#steps"
 	`);
