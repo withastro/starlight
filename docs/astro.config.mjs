a1ddc049ea7d076aa668ae67106b3b39c03870a2
Cabbage <1271246683@qq.com>
2024-03-18 15:30:41 +0000

    fix: fix the worng config lang

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -10,7 +10,7 @@ export const locales = {
 	fr: { label: 'Français', lang: 'fr' },
 	it: { label: 'Italiano', lang: 'it' },
 	id: { label: 'Bahasa Indonesia', lang: 'id' },
-	'zh-': { label: '简体中文', lang: 'zh-CN' },
+	'zh-cn': { label: '简体中文', lang: 'zh-CN' },
 	'pt-br': { label: 'Português do Brasil', lang: 'pt-BR' },
 	'pt-pt': { label: 'Português', lang: 'pt-PT' },
 	ko: { label: '한국어', lang: 'ko' },
