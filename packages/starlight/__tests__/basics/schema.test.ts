5f65bf31d96ccd78ef5af2a1cd963deecbda5e98
liruifengv <liruifeng1024@gmail.com>
2024-04-10 16:08:14 +0800

    Add TitleTransformConfigSchema tests in schema.test.ts

--- packages/starlight/__tests__/basics/schema.test.ts
+++ packages/starlight/__tests__/basics/schema.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, test } from 'vitest';
 import { FaviconSchema } from '../../schemas/favicon';
+import { TitleTransformConfigSchema } from '../../schemas/title'
 
 describe('FaviconSchema', () => {
 	test('returns the proper href and type attributes', () => {
@@ -14,4 +15,46 @@ describe('FaviconSchema', () => {
 	test('throws on invalid favicon extensions', () => {
 		expect(() => FaviconSchema().parse('/favicon.pdf')).toThrow();
 	});
+});
+
+
+describe('TitleTransformConfigSchema', () => {
+  test('title can be a string', () => {
+    const title = 'My Site';
+    const defaultLang = 'en'
+
+    const siteTitle = TitleTransformConfigSchema(defaultLang).parse(title);
+
+    expect(siteTitle).toEqual({
+      en: title
+    });
+  });
+
+  test('title can be an object', () => {
+    const title = {
+      en: 'My Site',
+      es: 'Mi Sitio'
+    };
+    const defaultLang = 'en'
+
+    const siteTitle = TitleTransformConfigSchema(defaultLang).parse(title);
+
+    expect(siteTitle).toEqual(title);
+  });
+
+  test('throws on empty object', () => {
+    const title = {};
+    const defaultLang = 'en'
+
+    expect(() => TitleTransformConfigSchema(defaultLang).parse(title)).toThrow();
+  });
+
+  test('throws on missing default language key', () => {
+    const title = {
+      es: 'Mi Sitio'
+    };
+    const defaultLang = 'en'
+
+    expect(() => TitleTransformConfigSchema(defaultLang).parse(title)).toThrow();
+  });
 });
