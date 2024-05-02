17ef3479780090830bcdc77faae22b561a2f3f89
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-05-02 12:03:40 +0200

    test: fix vitest configuration for `@astrojs/starlight-tailwind`

--- packages/tailwind/vitest.config.ts
+++ packages/tailwind/vitest.config.ts
@@ -4,11 +4,13 @@ export default defineConfig({
 	test: {
 		coverage: {
 			reportsDirectory: './__coverage__',
-			thresholdAutoUpdate: true,
-			lines: 100,
-			functions: 100,
-			branches: 100,
-			statements: 100,
+			thresholds: {
+				autoUpdate: true,
+				lines: 100,
+				functions: 100,
+				branches: 100,
+				statements: 100,
+			},
 		},
 	},
 });
