474395b2804bd68ed37997c32a6697086e780152
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2024-05-06 23:19:07 -0400

    rfc: BadgeComponent to support `attrs`

--- packages/starlight/schemas/badge.ts
+++ packages/starlight/schemas/badge.ts
@@ -1,19 +1,31 @@
 import { z } from 'astro/zod';
+import type { AstroBuiltinAttributes } from 'astro';
+import type { HTMLAttributes } from 'astro/types';
 
-const badgeSchema = () =>
-	z.object({
-		variant: z.enum(['note', 'danger', 'success', 'caution', 'tip', 'default']).default('default'),
-		text: z.string(),
+const spanHTMLAttributesSchema = z.record(
+	z.union([z.string(), z.number(), z.boolean(), z.undefined()])
+) as z.Schema<Omit<HTMLAttributes<'span'>, keyof AstroBuiltinAttributes | 'children'>>;
+
+const defaultSpanHTMLAttributesSchema = () => spanHTMLAttributesSchema.default({});
+
+const badgeSchema = z.object({
+	variant: z.enum(['note', 'danger', 'success', 'caution', 'tip', 'default']).default('default'),
+	text: z.string(),
+});
+
+const badgeSideBarSchema = () =>
+	badgeSchema.extend({
 		class: z.string().optional(),
 	});
 
-export const BadgeComponentSchema = badgeSchema().extend({
+export const BadgeComponentSchema = badgeSchema.extend({
 	size: z.enum(['small', 'medium', 'large']).default('small'),
+	attrs: defaultSpanHTMLAttributesSchema(),
 });
 
 export const BadgeConfigSchema = () =>
 	z
-		.union([z.string(), badgeSchema()])
+		.union([z.string(), badgeSideBarSchema()])
 		.transform((badge) => {
 			if (typeof badge === 'string') {
 				return { variant: 'default' as const, text: badge };
@@ -22,5 +34,7 @@ export const BadgeConfigSchema = () =>
 		})
 		.optional();
 
-export type Badge = z.output<ReturnType<typeof badgeSchema>>;
-export type BadgeComponent = z.output<typeof BadgeComponentSchema>;
+export type Badge = z.output<ReturnType<typeof badgeSideBarSchema>>;
+export type BadgeProps = Partial<Omit<z.output<typeof BadgeComponentSchema>, 'text'>> & {
+	text: string;
+};
