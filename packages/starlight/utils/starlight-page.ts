44ca490f36ecd6789d0865dd2506e5ea03dfa0fb
Chris Swithinbank <swithinbank@gmail.com>
2024-06-12 15:02:53 +0200

    [i18nIgnore] Update dependencies (#2007)

--- packages/starlight/utils/starlight-page.ts
+++ packages/starlight/utils/starlight-page.ts
@@ -292,10 +292,10 @@ type RemoveIndexSignature<T> = {
 	[K in keyof T as string extends K
 		? never
 		: number extends K
-		? never
-		: symbol extends K
-		? never
-		: K]: T[K];
+			? never
+			: symbol extends K
+				? never
+				: K]: T[K];
 };
 
 // https://www.totaltypescript.com/concepts/the-prettify-helper
