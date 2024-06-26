e7b723108dba8763642bb63aa3d994d3eb0a7796
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-06-26 15:45:35 +0200

    style: fix typos in test names

--- packages/starlight/__tests__/remark-rehype/asides.test.ts
+++ packages/starlight/__tests__/remark-rehype/asides.test.ts
@@ -173,7 +173,7 @@ test('runs without locales config', async () => {
 	expect(res.code.includes('aria-label=Note"'));
 });
 
-test('tranforms back unhandled text directives', async () => {
+test('transforms back unhandled text directives', async () => {
 	const res = await processor.render(
 		`This is a:test of a sentence with a text:name[content]{key=val} directive.`
 	);
@@ -184,7 +184,7 @@ test('tranforms back unhandled text directives', async () => {
 	`);
 });
 
-test('tranforms back unhandled leaf directives', async () => {
+test('transforms back unhandled leaf directives', async () => {
 	const res = await processor.render(`::video[Title]{v=xxxxxxxxxxx}`);
 	expect(res.code).toMatchInlineSnapshot(`
 		"<p>::video[Title]{v="xxxxxxxxxxx"}
