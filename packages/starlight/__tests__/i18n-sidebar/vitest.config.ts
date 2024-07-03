97121362e345eb65dd84c44b9f2052bb8dddb6eb
Chris Swithinbank <swithinbank@gmail.com>
2024-07-04 00:43:25 +0200

    Fix test config

--- packages/starlight/__tests__/i18n-sidebar/vitest.config.ts
+++ packages/starlight/__tests__/i18n-sidebar/vitest.config.ts
@@ -2,10 +2,9 @@ import { defineVitestConfig } from '../test-config';
 
 export default defineVitestConfig({
 	title: 'i18n sidebar',
-	defaultLocale: 'en',
 	locales: {
 		fr: { label: 'French' },
-		en: { label: 'English', lang: 'en-US' },
+		root: { label: 'English', lang: 'en-US' },
 	},
 	sidebar: [
 		{ slug: 'index' },
