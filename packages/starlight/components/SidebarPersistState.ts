74340a6817d21f91250b567df8c235b79928ed61
Chris Swithinbank <swithinbank@gmail.com>
2024-07-25 00:46:42 +0200

    Initial draft proposal

---
+++ packages/starlight/components/SidebarPersistState.ts
@@ -0,0 +1,22 @@
+// Collect required elements from the DOM.
+const target = document.querySelector<HTMLElement>('sl-sidebar-state-persist');
+const scroller = document.getElementById('starlight__sidebar');
+const details = target?.querySelectorAll('details') || [];
+
+/** Stores sidebar state as JSON in session storage. */
+const storeState = () => {
+	const state = {
+		hash: target?.dataset.hash || '',
+		open: [...details].map((element) => element.open),
+		scroll: scroller?.scrollTop || 0,
+	};
+	sessionStorage.setItem('sl-sidebar-state', JSON.stringify(state));
+};
+
+// Store sidebar state before navigating. These will also store it on tab blur etc.,
+// but avoid using the `beforeload` event, which can cause issues with back/forward cache
+// on some browsers.
+addEventListener('visibilitychange', () => {
+	if (document.visibilityState === 'hidden') storeState();
+});
+addEventListener('pageHide', storeState);
--- packages/starlight/utils/navigation.ts
+++ packages/starlight/utils/navigation.ts
@@ -344,6 +344,28 @@ export function getSidebar(pathname: string, locale: string | undefined): Sideba
 	}
 }
 
+/** Generates a deterministic string based on the content of the passed sidebar. */
+export function getSidebarHash(sidebar: SidebarEntry[]): string {
+	let hash = 0;
+	const sidebarIdentity = recursivelyBuildSidebarIdentity(sidebar);
+	for (let i = 0; i < sidebarIdentity.length; i++) {
+		const char = sidebarIdentity.charCodeAt(i);
+		hash = (hash << 5) - hash + char;
+	}
+	return (hash >>> 0).toString(36).padStart(7, '0');
+}
+
+/** Recurses through a sidebar tree to generate a string concatenating labels and link hrefs. */
+function recursivelyBuildSidebarIdentity(sidebar: SidebarEntry[]): string {
+	return sidebar
+		.flatMap((entry) =>
+			entry.type === 'group'
+				? entry.label + recursivelyBuildSidebarIdentity(entry.entries)
+				: entry.label + entry.href
+		)
+		.join('');
+}
+
 /** Turn the nested tree structure of a sidebar into a flat list of all the links. */
 export function flattenSidebar(sidebar: SidebarEntry[]): Link[] {
 	return sidebar.flatMap((entry) =>
