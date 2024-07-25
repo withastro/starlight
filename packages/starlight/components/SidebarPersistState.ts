62fd83d009eb6fc3bc5f14e296ddbca9b65da092
Chris Swithinbank <swithinbank@gmail.com>
2024-07-25 15:55:25 +0200

    Fix typo in comment

--- packages/starlight/components/SidebarPersistState.ts
+++ packages/starlight/components/SidebarPersistState.ts
@@ -14,7 +14,7 @@ const storeState = () => {
 };
 
 // Store sidebar state before navigating. These will also store it on tab blur etc.,
-// but avoid using the `beforeload` event, which can cause issues with back/forward cache
+// but avoid using the `beforeunload` event, which can cause issues with back/forward cache
 // on some browsers.
 addEventListener('visibilitychange', () => {
 	if (document.visibilityState === 'hidden') storeState();
