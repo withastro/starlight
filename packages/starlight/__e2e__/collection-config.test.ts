808a30adf5ba45fb2a3045bd348257286e8877ac
Chris Swithinbank <swithinbank@gmail.com>
2024-07-27 02:13:30 +0200

    Remove unit test, move e2e test to directory with spaces

--- packages/starlight/__e2e__/collection-config.test.ts
+++ packages/starlight/__e2e__/collection-config.test.ts
@@ -1,6 +1,6 @@
 import { expect, testFactory } from './test-utils';
 
-const test = await testFactory('./fixtures/custom-src-dir/');
+const test = await testFactory('./fixtures/custom src-dir/');
 
 test('builds a custom page using the `<StarlightPage>` component and a custom `srcDir`', async ({
 	page,
