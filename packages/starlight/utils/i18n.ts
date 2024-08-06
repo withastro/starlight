f96e84c39854190abd33ef3e1692fa27f44e108c
Chris Swithinbank <swithinbank@gmail.com>
2024-08-06 11:24:13 +0200

    Define well-known RTL locales before calling `getLocaleInfo()` for default locale

--- packages/starlight/utils/i18n.ts
+++ packages/starlight/utils/i18n.ts
@@ -2,9 +2,6 @@ import type { AstroConfig } from 'astro';
 import { AstroError } from 'astro/errors';
 import type { StarlightConfig } from './user-config';
 
-/** Informations about the built-in default locale used as a fallback when no locales are defined. */
-export const BuiltInDefaultLocale = { ...getLocaleInfo('en'), lang: 'en' };
-
 /**
  * A list of well-known right-to-left languages used as a fallback when determining the text
  * direction of a locale is not supported by the `Intl.Locale` API in the current environment.
@@ -13,6 +10,9 @@ export const BuiltInDefaultLocale = { ...getLocaleInfo('en'), lang: 'en' };
  * @see https://en.wikipedia.org/wiki/IETF_language_tag#List_of_common_primary_language_subtags
  */
 const wellKnownRTL = ['ar', 'fa', 'he', 'prs', 'ps', 'syc', 'ug', 'ur'];
+
+/** Informations about the built-in default locale used as a fallback when no locales are defined. */
+export const BuiltInDefaultLocale = { ...getLocaleInfo('en'), lang: 'en' };
 
 /**
  * Processes the Astro and Starlight i18n configurations to generate/update them accordingly:
