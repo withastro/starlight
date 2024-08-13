8cb86b5a1f45814b1fb6b5f01a20a66c66df7176
Chris Swithinbank <swithinbank@gmail.com>
2024-08-13 18:14:44 +0200

    Refactor persistence script to use click handler instead of toggle events

--- packages/starlight/components/SidebarPersistState.ts
+++ packages/starlight/components/SidebarPersistState.ts
@@ -1,32 +1,65 @@
 // Collect required elements from the DOM.
-const target = document.querySelector<HTMLElement>('sl-sidebar-state-persist');
 const scroller = document.getElementById('starlight__sidebar');
-const details = target?.querySelectorAll('details') || [];
-const hash = target?.dataset.hash || '';
+const target = scroller?.querySelector<HTMLElement>('sl-sidebar-state-persist');
+const details = [...(target?.querySelectorAll('details') || [])];
 
-/** Load the stored state from session storage. */
-const loadState = (): { open: Array<boolean | null> } => {
-	const rawStoredState = sessionStorage.getItem('sl-sidebar-state');
-	return rawStoredState ? JSON.parse(rawStoredState) : { open: [] };
+/** Starlight uses this key to store sidebar state in `sessionStorage`. */
+const storageKey = 'sl-sidebar-state';
+
+/** The shape used to persist sidebar state across a user’s session. */
+interface SidebarState {
+	hash: string;
+	open: Array<boolean | null>;
+	scroll: number;
+}
+
+/**
+ * Get the current sidebar state.
+ *
+ * The `open` state is loaded from session storage, while `scroll` and `hash` are read from the current page.
+ */
+const getState = (): SidebarState => {
+	let open = [];
+	try {
+		const rawStoredState = sessionStorage.getItem(storageKey);
+		const storedState = JSON.parse(rawStoredState || '{}');
+		if (Array.isArray(storedState.open)) open = storedState.open;
+	} catch {}
+	return {
+		hash: target?.dataset.hash || '',
+		open,
+		scroll: scroller?.scrollTop || 0,
+	};
 };
 
-/** Store the sidebar state to session storage.  */
-const storeState = ({ open }: { open: Array<boolean | null> }) => {
-	const state = { hash, open, scroll: scroller?.scrollTop || 0 };
-	sessionStorage.setItem('sl-sidebar-state', JSON.stringify(state));
+/** Store the passed sidebar state in session storage. */
+const storeState = (state: SidebarState): void => {
+	try {
+		sessionStorage.setItem(storageKey, JSON.stringify(state));
+	} catch {}
 };
 
 /** Updates sidebar state in session storage without modifying `open` state. */
-const updateState = () => storeState(loadState());
+const updateState = (): void => storeState(getState());
+
+/** Updates sidebar state in session storage to include a new value for a specific `<details>` element. */
+const setToggleState = (open: boolean, detailsIndex: number): void => {
+	const state = getState();
+	state.open[detailsIndex] = open;
+	storeState(state);
+};
 
 // Store the current `open` state whenever a user interacts with one of the `<details>` groups.
-details.forEach((el, index) =>
-	el.addEventListener('toggle', function () {
-		const state = loadState();
-		state.open[index] = this.open;
-		storeState(state);
-	})
-);
+target?.addEventListener('click', (event) => {
+	if (!(event.target instanceof Element)) return;
+	// Query for the nearest `<summary>` and then its parent `<details>`.
+	// This excludes clicks outside of the `<summary>`, which don’t trigger toggles.
+	const toggledDetails = event.target.closest('summary')?.closest('details');
+	if (!toggledDetails) return;
+	const index = details.indexOf(toggledDetails);
+	if (index === -1) return;
+	setToggleState(!toggledDetails.open, index);
+});
 
 // Store sidebar state before navigating. These will also store it on tab blur etc.,
 // but avoid using the `beforeunload` event, which can cause issues with back/forward cache
