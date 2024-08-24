c7e9f345bb601f631ad3bb60849564c4af9e63e9
Luiz Ferraz <luiz@lferraz.com>
2024-08-24 15:36:11 -0300

    Threshold rounding error

--- packages/starlight/vitest.config.ts
+++ packages/starlight/vitest.config.ts
@@ -1,31 +1,31 @@
 import { coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
-				test: {
-								coverage: {
-												all: true,
-												reportsDirectory: './__coverage__',
-												exclude: [
-																...coverageConfigDefaults.exclude,
-																'**/__e2e__/**',
-																'playwright.config.*',
-																'**/vitest.*',
-																'components.ts',
-																'types.ts',
-																// We use this to set up test environments so it isn‘t picked up, but we are testing it downstream.
-																'integrations/virtual-user-config.ts',
-																// Types-only export.
-																'props.ts',
-																// Main integration entrypoint — don’t think we’re able to test this directly currently.
-																'index.ts',
-												],
-												thresholds: {
-																autoUpdate: true,
-																lines: 89.26,
-																functions: 93.44,
-																branches: 93.15,
-																statements: 89.26,
-												},
-								},
-				},
+	test: {
+		coverage: {
+			all: true,
+			reportsDirectory: './__coverage__',
+			exclude: [
+				...coverageConfigDefaults.exclude,
+				'**/__e2e__/**',
+				'playwright.config.*',
+				'**/vitest.*',
+				'components.ts',
+				'types.ts',
+				// We use this to set up test environments so it isn‘t picked up, but we are testing it downstream.
+				'integrations/virtual-user-config.ts',
+				// Types-only export.
+				'props.ts',
+				// Main integration entrypoint — don’t think we’re able to test this directly currently.
+				'index.ts',
+			],
+			thresholds: {
+				autoUpdate: true,
+				lines: 89.26,
+				functions: 93.44,
+				branches: 93.14,
+				statements: 89.26,
+			},
+		},
+	},
 });
