9c2d47f03609165c4e731c99dbcdce72227f9700
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-08-28 14:14:27 +0200

    feat: use `extends` syntax

--- examples/markdoc/markdoc.config.mjs
+++ examples/markdoc/markdoc.config.mjs
@@ -2,5 +2,5 @@ import { defineMarkdocConfig } from '@astrojs/markdoc/config';
 import starlightMarkdoc from '@astrojs/starlight-markdoc';
 
 export default defineMarkdocConfig({
-	...starlightMarkdoc(),
+	extends: [starlightMarkdoc()],
 });
