bedbc1c37b95d0a7b14cb9a1057425d7cde72128
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-09-18 10:01:40 +0200

    feat: add `component.preview` ui label

--- docs/src/content/config.ts
+++ docs/src/content/config.ts
@@ -1,7 +1,14 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
 	docs: defineCollection({ schema: docsSchema() }),
-	i18n: defineCollection({ type: 'data', schema: i18nSchema() }),
+	i18n: defineCollection({
+		type: 'data',
+		schema: i18nSchema({
+			extend: z.object({
+				'component.preview': z.string().optional(),
+			}),
+		}),
+	}),
 };
