e0f2870ee66a7c0685ee9465938326a3bfb776ed
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-10-22 16:33:01 +0200

    ci: increase a11y build step timeout

--- packages/starlight/playwright.config.a11y.ts
+++ packages/starlight/playwright.config.a11y.ts
@@ -4,6 +4,7 @@ import config from './playwright.config';
 export default defineConfig({
 	...config,
 	testMatch: '__a11y__/*.test.ts',
+	// The timeout for the accessibility tests only.
 	timeout: 180 * 1_000,
 	webServer: [
 		{
@@ -11,6 +12,8 @@ export default defineConfig({
 			cwd: '../../docs',
 			reuseExistingServer: !process.env['CI'],
 			stdout: 'pipe',
+			// The timeout of the single build step ran before the accessibility tests.
+			timeout: 120 * 1_000,
 			url: 'http://localhost:4321',
 		},
 	],
