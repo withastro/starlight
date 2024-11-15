234c01dfdf58ea0221f8c2c79988ba45272c2cc8
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-11-15 16:53:02 +0100

    fix: loader with custom `srcDir`

--- packages/starlight/loader.ts
+++ packages/starlight/loader.ts
@@ -1,4 +1,4 @@
-import { glob, type Loader } from 'astro/loaders';
+import { glob, type Loader, type LoaderContext } from 'astro/loaders';
 
 // https://github.com/withastro/astro/blob/main/packages/astro/src/core/constants.ts#L87
 // https://github.com/withastro/astro/blob/main/packages/integrations/mdx/src/index.ts#L59
@@ -14,8 +14,8 @@ const i18nExtensions = ['json', 'yml', 'yaml'];
 // - Remark plugins get passed down an absolute path to a content file and we need to figure out
 //   the language from that path. Without knowing the content folder path, we cannot reliably do
 //   so.
-const docsBase = './src/content/docs';
-const i18nBase = './src/content/i18n';
+const docsBaseInSrcDir = 'content/docs';
+const i18nBaseInSrcDir = 'content/i18n';
 
 // TODO(HiDeoo) Do we need to support Markdoc or does the integration still handle that for us?
 // https://github.com/withastro/astro/blob/next/packages/integrations/markdoc/src/index.ts#L28
@@ -23,17 +23,22 @@ const i18nBase = './src/content/i18n';
 export function docsLoader(): Loader {
 	return {
 		name: 'starlight-docs-loader',
-		load: createGlobLoadFn(docsBase, docsExtensions),
+		load: createGlobLoadFn(docsBaseInSrcDir, docsExtensions),
 	};
 }
 
 export function i18nLoader(): Loader {
 	return {
 		name: 'starlight-i18n-loader',
-		load: createGlobLoadFn(i18nBase, i18nExtensions),
+		load: createGlobLoadFn(i18nBaseInSrcDir, i18nExtensions),
 	};
 }
 
-function createGlobLoadFn(base: string | URL, extensions: string[]): Loader['load'] {
-	return glob({ base, pattern: `**/[^_]*.{${extensions.join(',')}}` }).load;
+function createGlobLoadFn(base: string, extensions: string[]): Loader['load'] {
+	return (context: LoaderContext) => {
+		return glob({
+			base: context.config.srcDir.pathname.replace(context.config.root.pathname, '') + base,
+			pattern: `**/[^_]*.{${extensions.join(',')}}`,
+		}).load(context);
+	};
 }
