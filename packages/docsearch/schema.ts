1e8edf1708e72c7f90e45ae26424f5ad4d2f7866
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2024-11-19 16:03:21 +0100

    chore: update jsdoc example

--- packages/docsearch/schema.ts
+++ packages/docsearch/schema.ts
@@ -7,13 +7,14 @@ import { z } from 'astro/zod';
  *
  * ```js
  * import { defineCollection } from 'astro:content';
+ * import { docsLoader, i18nLoader } from '@astrojs/starlight/loader';
  * import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
  * import { docSearchI18nSchema } from '@astrojs/starlight-docsearch/schema';
  *
  * export const collections = {
- * 		docs: defineCollection({ schema: docsSchema() }),
+ * 		docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
  * 		i18n: defineCollection({
- * 			type: 'data',
+ * 			loader: i18nLoader(),
  * 			schema: i18nSchema({ extend: docSearchI18nSchema() }),
  * 		}),
  * };
