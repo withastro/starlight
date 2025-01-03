336f19da92366d933d23a5edd6bfdf3c1c793d96
Brian Montgomery <brian@accessiblecommunity.org>
2025-01-03 10:58:51 -0500

    Expose navigation functions via the internal module.

--- packages/starlight/internal.ts
+++ packages/starlight/internal.ts
@@ -1,6 +1,7 @@
 /**
- * @file This file contains utility functions imported by the `<Code>` component.
+ * @file This file contains utility functions imported by the `<Code>` and `<Sidebar>` components.
  */
 
 export { useTranslations } from './utils/translations';
 export { getStarlightEcConfigPreprocessor } from './integrations/expressive-code';
+export { getSidebar, getSidebarFromConfig } from './utils/navigation';
