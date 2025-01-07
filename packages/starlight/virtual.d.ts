067640b4a8cd46919e19035f6cabcd0e8c80b9bf
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-01-07 10:02:46 +0100

    fix: translations of languages with a region subtag

--- packages/starlight/virtual.d.ts
+++ packages/starlight/virtual.d.ts
@@ -7,3 +7,16 @@ declare module 'virtual:starlight/plugin-translations' {
 	const PluginTranslations: import('./utils/plugins').PluginTranslations;
 	export default PluginTranslations;
 }
+
+declare module 'virtual:starlight/project-context' {
+	const ProjectContext: {
+		root: string;
+		srcDir: string;
+		trailingSlash: import('astro').AstroConfig['trailingSlash'];
+		build: {
+			format: import('astro').AstroConfig['build']['format'];
+		};
+		legacyCollections: boolean;
+	};
+	export default ProjectContext;
+}
