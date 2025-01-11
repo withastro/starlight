eb0357f8c677d51c4c4289cbcc2ce48afd00b446
Chris Swithinbank <swithinbank@gmail.com>
2025-01-11 01:04:23 +0100

    Deprecate the old override props export

--- packages/starlight/props.ts
+++ packages/starlight/props.ts
@@ -1,2 +1,15 @@
-export type { StarlightRouteData as Props } from './utils/routing/types';
+export type {
+	/**
+	 * @deprecated The `Props` type is deprecated. If updating an override to use
+	 * `Astro.locals.starlightRoute` instead of `Astro.props`, import the new `StarlightRouteData`
+	 * type instead:
+	 * ```astro
+	 * ---
+	 * import type { StarlightRouteData } from '@astrojs/starlight/route-data';
+	 * ---
+	 * ```
+	 */
+	StarlightRouteData as Props,
+} from './utils/routing/types';
+
 export type { StarlightPageProps } from './utils/starlight-page';
