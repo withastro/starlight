6b0546ca2b85525644709fe64216db7bf0341dea
Emilien GUILMINEAU <guilmineau.emilien@gmail.com>
2025-02-04 21:08:20 +0100

    wip: Add missing properties for mergeIndex

--- packages/starlight/schemas/pagefind.ts
+++ packages/starlight/schemas/pagefind.ts
@@ -1,6 +1,79 @@
 import { z } from 'astro/zod';
 
 const indexWeightSchema = z.number().nonnegative().nullish();
+const pagefindRankingWeightsSchema = z
+	.object({
+		/**
+		 * Set Pagefind’s `pageLength` ranking option.
+		 *
+		 * The default value is `0.1` and values must be in the range `0` to `1`.
+		 *
+		 * @see https://pagefind.app/docs/ranking/#configuring-page-length
+		 */
+		pageLength: z.number().min(0).max(1).default(0.1),
+		/**
+		 * Set Pagefind’s `termFrequency` ranking option.
+		 *
+		 * The default value is `0.1` and values must be in the range `0` to `1`.
+		 *
+		 * @see https://pagefind.app/docs/ranking/#configuring-term-frequency
+		 */
+		termFrequency: z.number().min(0).max(1).default(0.1),
+		/**
+		 * Set Pagefind’s `termSaturation` ranking option.
+		 *
+		 * The default value is `2` and values must be in the range `0` to `2`.
+		 *
+		 * @see https://pagefind.app/docs/ranking/#configuring-term-saturation
+		 */
+		termSaturation: z.number().min(0).max(2).default(2),
+		/**
+		 * Set Pagefind’s `termSimilarity` ranking option.
+		 *
+		 * The default value is `9` and values must be greater than or equal to `0`.
+		 *
+		 * @see https://pagefind.app/docs/ranking/#configuring-term-similarity
+		 */
+		termSimilarity: z.number().min(0).default(9),
+	});
+const pagefindIndexOptionsSchema = z.object({
+    /** 
+	 * Overrides the URL path that Pagefind uses to load its search bundle
+	 * */
+	basePath: z.string().nullish(),
+    /**
+	 * Appends the given baseURL to all search results. May be a path, or a full domain
+	 * */
+	baseUrl: z.string().nullish(),
+    /**
+	 *  The maximum length of excerpts that Pagefind should generate for search results.
+     *
+     * Only applies in multisite setups, where one site should rank higher or lower than others.
+	 * */
+	excerptLength: z.number().nonnegative().nullish(),
+    /**
+     * Multiply all rankings for this index by the given weight.
+     */
+	indexWeight: indexWeightSchema,
+    /**
+     * Merge this filter object into all search queries in this index.
+     *
+     * Only applies in multisite setups.
+     */
+	mergeFilter: z.any().nullish(),
+    /**
+     * If set, will ass the search term as a query parameter under this key, for use with Pagefind's highlighting script.
+     */
+	highlightParam: z.string().nullish(),
+    /**
+     * Language of this index.
+     */
+	language: z.string().nullish(),
+    /**
+     * Provides the ability to fine tune Pagefind's ranking algorithm to better suit your dataset.
+     */
+	ranking: pagefindRankingWeightsSchema.nullish(),
+});
 
 const pagefindSchema = z.object({
 	/**
@@ -13,42 +86,7 @@ const pagefindSchema = z.object({
 	 */
 	indexWeight: indexWeightSchema,
 	/** Configure how search result rankings are calculated by Pagefind. */
-	ranking: z
-		.object({
-			/**
-			 * Set Pagefind’s `pageLength` ranking option.
-			 *
-			 * The default value is `0.1` and values must be in the range `0` to `1`.
-			 *
-			 * @see https://pagefind.app/docs/ranking/#configuring-page-length
-			 */
-			pageLength: z.number().min(0).max(1).default(0.1),
-			/**
-			 * Set Pagefind’s `termFrequency` ranking option.
-			 *
-			 * The default value is `0.1` and values must be in the range `0` to `1`.
-			 *
-			 * @see https://pagefind.app/docs/ranking/#configuring-term-frequency
-			 */
-			termFrequency: z.number().min(0).max(1).default(0.1),
-			/**
-			 * Set Pagefind’s `termSaturation` ranking option.
-			 *
-			 * The default value is `2` and values must be in the range `0` to `2`.
-			 *
-			 * @see https://pagefind.app/docs/ranking/#configuring-term-saturation
-			 */
-			termSaturation: z.number().min(0).max(2).default(2),
-			/**
-			 * Set Pagefind’s `termSimilarity` ranking option.
-			 *
-			 * The default value is `9` and values must be greater than or equal to `0`.
-			 *
-			 * @see https://pagefind.app/docs/ranking/#configuring-term-similarity
-			 */
-			termSimilarity: z.number().min(0).default(9),
-		})
-		.default({}),
+	ranking: pagefindRankingWeightsSchema.default({}),
 	/**
 	 * Configure how search indexes from different sites are merged by Pagefind.
 	 *
@@ -56,20 +94,23 @@ const pagefindSchema = z.object({
 	 */
 	mergeIndex: z
 		.array(
-			z.object({
+			/**
+			 * Each entry of this array represents a `PagefindIndexOptions` from pagefind.
+			 *
+			 * @see https://github.com/CloudCannon/pagefind/blob/v1.3.0/pagefind_web_js/lib/coupled_search.ts#L549
+			 */
+			pagefindIndexOptionsSchema.extend({
 				/**
 				 * Set Pagefind’s `bundlePath` mergeIndex option.
+				 * 
+				 * This option is not part of the `PagefindIndexOptions` type on pagefind, 
+				 * but it's presence is checked before using it on the pagefind_ui module.
 				 *
 				 * @see https://pagefind.app/docs/multisite/#searching-additional-sites-from-pagefind-ui
+				 * @see https://github.com/CloudCannon/pagefind/blob/v1.3.0/pagefind_ui/modular/modular-core.js#L192
 				 */
-				pageLength: z.string(),
-				/**
-				 * Set Pagefind’s `indexWeight` mergeIndex option.
-				 *
-				 * @see https://pagefind.app/docs/multisite/#searching-additional-sites-from-pagefind-ui
-				 */
-				indexWeight: indexWeightSchema,
-			}),
+				bundlePath: z.string(),
+			})
 		)
 		.nullish(),
 });
