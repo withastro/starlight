54bda59e6573b430d114dc37c6d0e9ccc694ef46
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-03-17 10:20:04 +0100

    refactor: merge `starlight.asides` & `starlight.expressive-code` in `starlight.components`

--- packages/starlight/integrations/expressive-code/index.ts
+++ packages/starlight/integrations/expressive-code/index.ts
@@ -137,7 +137,7 @@ export function getStarlightEcConfigPreprocessor({
 				// Return the default selector
 				return `[data-theme='${theme.name}']`;
 			},
-			cascadeLayer: cascadeLayer ?? 'starlight.expressive-code',
+			cascadeLayer: cascadeLayer ?? 'starlight.components',
 			styleOverrides: {
 				borderRadius: '0px',
 				borderWidth: '1px',
