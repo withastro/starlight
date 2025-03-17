aa3b1ee86cb65e655c8d41146af137c13201aac5
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-03-17 11:35:41 +0100

    test: update tailwind tests

--- packages/tailwind/__tests__/tailwind.test.ts
+++ packages/tailwind/__tests__/tailwind.test.ts
@@ -9,7 +9,7 @@ describe('@layer base', async () => {
 	const output = await render();
 	const baseLayer = output.match(/@layer base {(.*?)}[\s\n]*@layer/s)?.[1];
 
-	test('generates base layer CSS', () => {
+	test('generates base and utilities CSS layers', () => {
 		// The first line includes Tailwind version so we skip it.
 		const outputWithoutVersion = output.split('\n').slice(1).join('\n');
 		expect(outputWithoutVersion).toMatchInlineSnapshot(`
@@ -23,7 +23,11 @@ describe('@layer base', async () => {
 			  code, kbd, samp, pre {
 			    font-family: var(--font-mono);
 			  }
+			}
 
+			@layer components;
+
+			@layer utilities {
 			  :root {
 			    --sl-font: var(--font-sans);
 			    --sl-font-mono: var(--font-mono);
@@ -55,8 +59,6 @@ describe('@layer base', async () => {
 			    }
 			  }
 			}
-
-			@layer components, utilities;
 			"
 		`);
 	});
@@ -66,11 +68,6 @@ describe('@layer base', async () => {
 		expect(baseLayer).toMatch(
 			/code, kbd, samp, pre {[\s\n]+font-family: var\(--font-mono\);[\s\n]+}/
 		);
-	});
-
-	test('configures `--sl-color-*` variables', () => {
-		expect(baseLayer).includes('--sl-color-gray-1: var(--color-gray-200);');
-		expect(baseLayer).includes('--sl-color-accent: var(--color-accent-600, #4f46e5);');
 	});
 });
 
@@ -85,12 +82,48 @@ describe('@layer utilities', async () => {
 	);
 	const utilitiesLayer = output.match(/@layer utilities {(.*?)}\n*$/s)?.[1];
 
+	test('configures `--sl-color-*` variables', () => {
+		expect(utilitiesLayer).includes('--sl-color-gray-1: var(--color-gray-200);');
+		expect(utilitiesLayer).includes('--sl-color-accent: var(--color-accent-600, #4f46e5);');
+	});
+
 	test('uses [data-theme="dark"] for dark: utility classes', async () => {
 		expect(utilitiesLayer).toMatchInlineSnapshot(`
 			"
 			  .dark\\:bg-black {
 			    &:where([data-theme="dark"], [data-theme="dark"] *) {
 			      background-color: var(--color-black);
+			    }
+			  }
+
+			  :root {
+			    --sl-font: var(--font-sans);
+			    --sl-font-mono: var(--font-mono);
+			    --sl-color-white: var(--color-white);
+			    --sl-color-gray-1: var(--color-gray-200);
+			    --sl-color-gray-2: var(--color-gray-300);
+			    --sl-color-gray-3: var(--color-gray-400);
+			    --sl-color-gray-4: var(--color-gray-600);
+			    --sl-color-gray-5: var(--color-gray-700);
+			    --sl-color-gray-6: var(--color-gray-800);
+			    --sl-color-black: var(--color-gray-900);
+			    --sl-color-accent-low: var(--color-accent-950, var(--color-accent-900, #1e1b4b));
+			    --sl-color-accent: var(--color-accent-600, #4f46e5);
+			    --sl-color-accent-high: var(--color-accent-200, #c7d2fe);
+
+			    &[data-theme="light"] {
+			      --sl-color-white: var(--color-gray-900);
+			      --sl-color-gray-1: var(--color-gray-800);
+			      --sl-color-gray-2: var(--color-gray-700);
+			      --sl-color-gray-3: var(--color-gray-500);
+			      --sl-color-gray-4: var(--color-gray-400);
+			      --sl-color-gray-5: var(--color-gray-300);
+			      --sl-color-gray-6: var(--color-gray-200);
+			      --sl-color-gray-7: var(--color-gray-100);
+			      --sl-color-black: var(--color-white);
+			      --sl-color-accent-low: var(--color-accent-200, #c7d2fe);
+			      --sl-color-accent: var(--color-accent-600, #4f46e5);
+			      --sl-color-accent-high: var(--color-accent-900, #312e81);
 			    }
 			  }
 			"
