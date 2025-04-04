a633ab4cd05dc4e1b6e298d74ada6c77f1ff4b5e
Chris Swithinbank <swithinbank@gmail.com>
2025-04-04 13:18:17 +0200

    Add anchor heading support to Markdoc preset

--- packages/markdoc/components.ts
+++ packages/markdoc/components.ts
@@ -1,1 +1,2 @@
 export { default as Code } from './Code.astro';
+export { default as Heading } from '@astrojs/starlight/components/AnchorHeading.astro';
--- packages/markdoc/index.mjs
+++ packages/markdoc/index.mjs
@@ -1,4 +1,4 @@
-import { component } from '@astrojs/markdoc/config';
+import { component, nodes } from '@astrojs/markdoc/config';
 import { WellKnownElementAttributes, WellKnownAnchorAttributes } from './html.mjs';
 
 /**
@@ -283,7 +283,23 @@ export const StarlightMarkdocPreset = {
 	},
 };
 
-/** @return {import('@astrojs/markdoc/config').AstroMarkdocConfig} */
-export default function starlightMarkdoc() {
-	return StarlightMarkdocPreset;
+/**
+ * Markdoc preset that configures Starlight’s built-in components.
+ * @return {import('@astrojs/markdoc/config').AstroMarkdocConfig}
+ */
+export default function starlightMarkdoc({ headingLinks = true } = {}) {
+	return {
+		...StarlightMarkdocPreset,
+		nodes: {
+			...StarlightMarkdocPreset.nodes,
+			...(headingLinks
+				? {
+						heading: {
+							...nodes.heading,
+							render: component('@astrojs/starlight-markdoc/components', 'Heading'),
+						},
+					}
+				: {}),
+		},
+	};
 }
