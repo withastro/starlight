b53da1f15c3c6029c1ffa11d2df0c305560fa0fd
Chris Swithinbank <swithinbank@gmail.com>
2025-04-10 22:29:20 +0200

    Merge anchor heading and tabs tests into single file
    Both of these are using `./fixtures/basics/`, which was causing race conditions that failed in CI.

--- packages/starlight/__e2e__/components.test.ts
+++ packages/starlight/__e2e__/components.test.ts
@@ -374,6 +374,25 @@ test.describe('whitespaces', () => {
 	});
 });
 
+test.describe('anchor headings', () => {
+	test('renders the same content for Markdown headings and Astro component', async ({
+		getProdServer,
+		page,
+	}) => {
+		const starlight = await getProdServer();
+
+		await starlight.goto('/anchor-heading');
+		const markdownContent = page.locator('.sl-markdown-content');
+		const markdownHtml = await markdownContent.innerHTML();
+
+		await starlight.goto('/anchor-heading-component');
+		const componentContent = page.locator('.sl-markdown-content');
+		const componentHtml = await componentContent.innerHTML();
+
+		expect(markdownHtml).toEqual(componentHtml);
+	});
+});
+
 async function expectSelectedTab(tabs: Locator, label: string, panel?: string) {
 	expect(
 		(
