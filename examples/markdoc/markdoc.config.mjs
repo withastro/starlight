5be85a905442dafe6edcfc04469f914b17adf9f0
Devansh Purohit <25509659+devd-99@users.noreply.github.com>
2025-05-06 13:30:16 -0400

    Remove unused example files

--- examples/markdoc/markdoc.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineMarkdocConfig } from '@astrojs/markdoc/config';
-import starlightMarkdoc from '@astrojs/starlight-markdoc';
-
-// https://docs.astro.build/en/guides/integrations-guide/markdoc/
-export default defineMarkdocConfig({
-	extends: [starlightMarkdoc()],
-});
