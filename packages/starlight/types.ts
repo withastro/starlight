c8a8d387a7d0ff892fa5d0a4d69cf2825cdcc8bf
Alvin Bryan <107407814+alvinometric@users.noreply.github.com>
2025-07-08 08:28:00 +0100

    Update types.ts

--- packages/starlight/types.ts
+++ packages/starlight/types.ts
@@ -5,4 +5,4 @@ export type {
 	HookParameters,
 } from './utils/plugins';
 export type { StarlightIcon } from './components/Icons';
-import type { SidebarEntry } from './utils/routing/types';
+export type { SidebarEntry } from './utils/routing/types';
