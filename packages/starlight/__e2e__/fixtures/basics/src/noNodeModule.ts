035291a83370c83031f33acda7906d1efc13de6a
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-07-25 18:58:11 +0200

    fix: node builtin dependencies

---
+++ packages/starlight/__e2e__/fixtures/basics/src/noNodeModule.ts
@@ -0,0 +1,34 @@
+import type { ViteUserConfig } from 'astro';
+
+/**
+ * A Vite plugin used to verify that the final bundle does not have a hard dependency on Node.js
+ * builtins due to Starlight.
+ * This is to ensure that Starlight can run on platforms like Cloudflare.
+ *
+ * @see https://github.com/withastro/astro/blob/8491aa56e8685677af8458ff1c5a80d6461413f8/packages/astro/test/test-plugins.js
+ */
+export function preventNodeBuiltinDependencyPlugin(): NonNullable<
+	ViteUserConfig['plugins']
+>[number] {
+	return {
+		name: 'verify-no-node-stuff',
+		generateBundle() {
+			const nodeModules = ['node:fs', 'node:url', 'node:worker_threads', 'node:path'];
+			nodeModules.forEach((name) => {
+				const importers = this.getModuleInfo(name)?.importers || [];
+				const starlightPath = new URL('../../../../', import.meta.url).pathname;
+				const nodeStarlightImport = importers.find((importer) =>
+					importer.startsWith(starlightPath)
+				);
+
+				if (nodeStarlightImport) {
+					throw new Error(
+						'A node builtin dependency imported by Starlight was found in the production bundle:\n\n' +
+							` - Node builtin: '${name}'\n` +
+							` - Importer: ${nodeStarlightImport}\n`
+					);
+				}
+			});
+		},
+	};
+}
---
+++ packages/starlight/utils/collection-fs.ts
@@ -0,0 +1,19 @@
+import { resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { getCollectionUrl, type StarlightCollection } from './collection';
+
+/**
+ * @see {@link file://./collection.ts} for more context about this file.
+ *
+ * Below are various functions to easily get paths to collections used in Starlight that rely on
+ * Node.js builtins. They exist in a separate file from {@link file://./collection.ts} to avoid
+ * to potentially importing Node.js builtins in the final bundle.
+ */
+
+export function resolveCollectionPath(collection: StarlightCollection, srcDir: URL) {
+	return resolve(fileURLToPath(srcDir), `content/${collection}`);
+}
+
+export function getCollectionPosixPath(collection: StarlightCollection, srcDir: URL) {
+	return fileURLToPath(getCollectionUrl(collection, srcDir)).replace(/\\/g, '/');
+}
