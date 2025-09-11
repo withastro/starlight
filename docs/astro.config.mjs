d03793190435f9018de309f4523e488b985e8ed8
aaron-c28 <aaron28@berkeley.edu>
2025-09-10 20:26:43 -0400

    revised_copy for most pages

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -86,6 +86,7 @@ export default defineConfig({
 						'how-it-works/7702-transactions',
 						'how-it-works/verified-signing',
 						'how-it-works/contracts',
+						'how-it-works/export-your-wallet',
 						'referrals/overview',
 					],
 				},
