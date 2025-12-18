cb6c2b54ada4681fd3b3efa2c293cf4aaa1984d7
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2025-12-18 16:48:11 +0100

    chore: add todo for `legacy.collectionsBackwardsCompat`

--- packages/starlight/__e2e__/legacy-collection-config-file.test.ts
+++ packages/starlight/__e2e__/legacy-collection-config-file.test.ts
@@ -1,5 +1,11 @@
 import { expect, testFactory } from './test-utils';
 
+// TODO(HiDeoo) This test and fixture should be updated to use the `legacy.collectionsBackwardsCompat` flag when available.
+// TODO(HiDeoo) @see https://github.com/withastro/astro/pull/14927
+// TODO(HiDeoo) When that is the case:
+// TODO(HiDeoo) 	- Rename the fixture
+// TODO(HiDeoo) 	- Update the comment below to reflect that the flag is being used.
+
 // This fixture uses a legacy collection config file (`src/content/config.ts`) instead of the new
 // one (`src/content.config.ts`).
 const test = testFactory('./fixtures/legacy-collection-config-file/');
