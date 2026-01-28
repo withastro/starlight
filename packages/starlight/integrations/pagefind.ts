3a6c667efb6b1ef6b89c972fef34f931a8495974
Luke Taylor <luke@deentaylor.com>
2026-01-28 17:33:26 -0500

    fix passing user config to pagefind integration

--- packages/starlight/integrations/pagefind.ts
+++ packages/starlight/integrations/pagefind.ts
@@ -1,13 +1,13 @@
 import type { HookParameters } from 'astro';
 import { fileURLToPath } from 'node:url';
 import * as pagefind from 'pagefind';
-import { pagefindUserConfig } from 'virtual:starlight/pagefind-config';
+import type { StarlightConfig } from '../types';
 
 /** Run Pagefind to generate search index files based on the build output directory. */
 export async function starlightPagefind({
 	dir,
 	logger: starlightLogger,
-}: PagefindIntegrationOptions) {
+}: PagefindIntegrationOptions, userConfig: Extract<StarlightConfig['pagefind'], object>) {
 	const logger = starlightLogger.fork('starlight:pagefind');
 	const options = { dir, logger };
 
@@ -15,7 +15,7 @@ export async function starlightPagefind({
 		const now = performance.now();
 		logger.info('Building search index with Pagefind...');
 
-		const newIndexResponse = await pagefind.createIndex(pagefindUserConfig.index);
+		const newIndexResponse = await pagefind.createIndex(userConfig.index);
 
 		const { index } = assertPagefindResponse<pagefind.NewIndexResponse>(newIndexResponse, options);
 
