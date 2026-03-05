84224b79f2dfb94ef3a7b1372be29bd440ccf37f
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-03-05 12:03:08 +0100

    chore: fix linting issue due to better vitest 4 types

--- packages/starlight/__tests__/remark-rehype/asides.test.ts
+++ packages/starlight/__tests__/remark-rehype/asides.test.ts
@@ -134,8 +134,6 @@ Some text
 			// We are not relying on `toThrowErrorMatchingInlineSnapshot()` and our custom snapshot
 			// serializer in this specific test as error thrown in a remark plugin includes a dynamic file
 			// path.
-			// `expect.objectContaining` returns `any`.
-			/* eslint-disable @typescript-eslint/no-unsafe-argument */
 			expect.objectContaining({
 				type: 'AstroUserError',
 				// `expect.stringMatching` returns `any`.
