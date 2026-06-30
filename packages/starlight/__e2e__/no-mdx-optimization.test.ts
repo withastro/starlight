7faf5885407fbe45639b7d7cc87643adc8136605
Chris Swithinbank <swithinbank@gmail.com>
2026-06-30 19:22:01 +0200

    Add comment with link to issue

--- packages/starlight/__e2e__/no-mdx-optimization.test.ts
+++ packages/starlight/__e2e__/no-mdx-optimization.test.ts
@@ -9,6 +9,10 @@ test.describe('no MDX optimization', () => {
 			await starlight.goto('/asides/');
 
 			const asideIconsLocator = page.locator('.starlight-aside__title svg');
+
+			// All four aside instances should have an icon, so we expect four icons to be rendered.
+			// This checks an issue where SVGs were escaped and rendered as text with MDX optimization
+			// disabled. See https://github.com/withastro/starlight/pull/3967#issuecomment-4833368829
 			await expect(asideIconsLocator).toHaveCount(4);
 		});
 	});
