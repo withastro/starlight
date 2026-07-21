59b9e33c5a1e484e4ac5f7fd4e3b72c7b03daecd
Chris Swithinbank <swithinbank@gmail.com>
2026-07-21 20:35:29 +0200

    Handle sidebar links by checking for any protocol component

--- packages/starlight/utils/url.ts
+++ packages/starlight/utils/url.ts
@@ -2,3 +2,6 @@ const HTTPProtocolRegEx = /^https?:\/\//;
 
 /** Check if a string starts with one of `http://` or `https://`. */
 export const isAbsoluteUrl = (link: string) => HTTPProtocolRegEx.test(link);
+
+/** Check if a string contains a protocol (e.g., `http:`, `https:`, `mailto:`). */
+export const hasProtocol = (link: string) => link.includes(':');
