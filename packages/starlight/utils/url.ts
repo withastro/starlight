34c46d859e11819582214e2a9fb36f23d2708bcd
Chris Swithinbank <swithinbank@gmail.com>
2026-07-22 00:23:11 +0200

    Use slower but more correct protocol check
    Co-authored-by: HiDeoo <494699+HiDeoo@users.noreply.github.com>

--- packages/starlight/utils/url.ts
+++ packages/starlight/utils/url.ts
@@ -1,12 +1,8 @@
 const HTTPProtocolRegEx = /^https?:\/\//;
+const ProtocolRegEx = /^[a-z][a-z\d+.-]*:/i;
 
 /** Check if a string starts with one of `http://` or `https://`. */
 export const isAbsoluteUrl = (link: string) => HTTPProtocolRegEx.test(link);
 
 /** Check if a string contains a protocol (e.g., `http:`, `https:`, `mailto:`). */
-export const hasProtocol = (link: string) => {
-	const colonIndex = link.indexOf(':');
-	if (colonIndex === -1) return false;
-	const slashIndex = link.indexOf('/');
-	return slashIndex === -1 || colonIndex < slashIndex;
-};
+export const hasProtocol = (link: string) => ProtocolRegEx.test(link);
