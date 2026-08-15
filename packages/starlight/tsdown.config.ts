b2f932c2c34e9ab2d174fda54e333514b3f67d37
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-08-15 19:44:10 +0200

    build: update tools and remove custom copy plugin

--- packages/starlight/tsdown.config.ts
+++ packages/starlight/tsdown.config.ts
@@ -1,12 +1,14 @@
 import fs from 'node:fs/promises';
-import path from 'node:path';
-import { glob } from 'tinyglobby';
 import { defineConfig } from 'tsdown';
 
 const outDir = 'dist';
 
 export default defineConfig({
-	copy: ['src/components.ts', 'src/style'],
+	checks: { pluginTimings: false },
+	copy: ['src/components.ts', 'src/style', { from: 'src/**/*.astro', flatten: false }],
+	deps: {
+		neverBundle: [/^astro:/, /^virtual:starlight\//],
+	},
 	dts: true,
 	entry: [
 		'src/**/*.ts',
@@ -14,9 +16,8 @@ export default defineConfig({
 		// https://github.com/withastro/astro/blob/c4c99aa7a5e8e45ada0efa4fda6e6fb96f334663/packages/astro/package.json#L55
 		'!src/components.ts',
 	],
-	external: [/^astro:/, /^virtual:starlight\//],
 	outDir,
-	plugins: [rawImportPlugin(), globCopyPlugin(['**/*.astro'], 'src')],
+	plugins: [rawImportPlugin()],
 	publint: { strict: true },
 	unbundle: true,
 });
@@ -43,55 +44,4 @@ function rawImportPlugin() {
 			return `export default ${JSON.stringify(content)}`;
 		},
 	};
-}
-
-/**
- * Plugin copying files matching given glob patterns to the output directory.
- *
- * The tsdown `copy` option does not support glob patterns, which can be useful in the case of
- * Astro files that we all want to copy over as-is. Specifying each directory containing Astro
- * files to the `copy` option would be tedious and would also end up copying non-Astro files, e.g.
- * TypeScript files in the `src/components/` directory that we do not want to copy as-is.
- */
-function globCopyPlugin(globs: string[], cwd: string) {
-	return {
-		name: 'glob-copy-plugin',
-		async writeBundle() {
-			let total = 0;
-
-			info('Copying files matching globs');
-
-			for (const pattern of globs) {
-				const files = await glob(pattern, { cwd });
-
-				await Promise.all(
-					files.map((file) => {
-						const from = path.resolve(cwd, file);
-						const to = path.resolve(outDir, file);
-						return fs.cp(from, to, { force: true });
-					})
-				);
-
-				total += files.length;
-
-				info(`${dim(`${cwd}/`)}${pattern} ${dim(`- ${files.length} files`)}`);
-			}
-
-			info(`${total} files`);
-		},
-	};
-}
-
-function info(message: string) {
-	console.info(`${blue('ℹ')} ${message}`);
-}
-
-// TODO: refactor logging to use `util.styleText()` when possible (22.13.0).
-function blue(message: string) {
-	return `\x1b[34m${message}\x1b[0m`;
-}
-
-// TODO: refactor logging to use `util.styleText()` when possible (22.13.0).
-function dim(message: string) {
-	return `\x1b[2m${message}\x1b[0m`;
 }
