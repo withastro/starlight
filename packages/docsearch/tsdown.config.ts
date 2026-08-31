46c968eea7bba108c83441e4b19004b4689f58a5
HiDeoo <494699+HiDeoo@users.noreply.github.com>
2026-08-31 12:28:58 +0200

    feat: disable `fixedExtension` for docsearch

--- packages/docsearch/tsdown.config.ts
+++ packages/docsearch/tsdown.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
 	copy: ['src/DocSearch.astro', 'src/variables.css'],
 	dts: true,
 	entry: ['src/index.ts', 'src/schema.ts'],
+	fixedExtension: false,
 	publint: { strict: true },
 	unbundle: true,
 });
